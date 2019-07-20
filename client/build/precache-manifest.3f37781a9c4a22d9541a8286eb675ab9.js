0ccc6d449dbbff75529135be1495167b752aedb3
Legionnaire <blake.marter@gmail.com>
2019-07-19 20:19:04 -0700

    fixing package.json

---
+++ client/build/precache-manifest.3f37781a9c4a22d9541a8286eb675ab9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0c04c9f7c3f099aad42",
+    "url": "/static/css/main.69d303f6.chunk.css"
+  },
+  {
+    "revision": "f0c04c9f7c3f099aad42",
+    "url": "/static/js/main.92a4d27c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4000236ed2bd87d5585b",
+    "url": "/static/js/2.2b2a1168.chunk.js"
+  },
+  {
+    "revision": "7064d45dc1155fde38295885e7a4455e",
+    "url": "/index.html"
+  }
+];
