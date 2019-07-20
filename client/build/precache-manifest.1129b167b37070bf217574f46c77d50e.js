7bb56474502e6841c15a1ce0953045a3523f050a
Legionnaire <blake.marter@gmail.com>
2019-07-19 18:45:21 -0700

    npm updates

---
+++ client/build/precache-manifest.1129b167b37070bf217574f46c77d50e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4d5d950c36e1890e005",
+    "url": "/static/css/main.69d303f6.chunk.css"
+  },
+  {
+    "revision": "c4d5d950c36e1890e005",
+    "url": "/static/js/main.47ee80ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4000236ed2bd87d5585b",
+    "url": "/static/js/2.2b2a1168.chunk.js"
+  },
+  {
+    "revision": "be881a545245b24c6b4e5eec141465c7",
+    "url": "/index.html"
+  }
+];
